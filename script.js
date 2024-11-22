0c0e7cd4e55a4831b0bf96fe90d0ac1c6779cd99
Riyanfebri <75291185+KEPET-MAKER@users.noreply.github.com>
2024-11-22 21:43:58 +0700

    Update script.js

--- script.js
+++ script.js
@@ -62,7 +62,7 @@ function drawWheel() {
     ctx.translate(canvas.width / 2, canvas.height / 2);
     ctx.rotate(angle + arc / 2);
 
-    ctx.drawImage(img, radius - 104, -70, 93, 93);
+    ctx.drawImage(img, radius - 104, -70, 90, 90);
 
     // Tampilkan keterangan gambar
     ctx.textAlign = "center";
