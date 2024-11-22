e61aff27459c0e1de11a66ef05e840f1bf3b54d5
Tugas-Kelompok-1 <tamara.23025@mhs.unesa.ac.id>
2024-11-23 06:39:22 +0700

    Update script.js

--- script.js
+++ script.js
@@ -117,7 +117,7 @@ canvas.addEventListener("click", (event) => {
   // Jika klik dalam radius tombol, mulai putaran
   if (distance <= 50) {
     if (!isSpin) {
-      spinAngle = Math.random() * 500 + 200; // Kecepatan putaran
+      spinAngle = Math.random() * 100 + 100; // Kecepatan putaran
       isSpin = true;
       rotateWheel();
     }
